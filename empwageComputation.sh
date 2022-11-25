2c9858114f3311af522fbf265b7bb421ac98b8a2
gitakshay1 <akshaypatil309p8@gmail.com>
2022-11-25 07:19:57 +0530

    Added: Calculating Wages for Month

--- empwageComputation.sh
+++ empwageComputation.sh
@@ -2,25 +2,32 @@ echo "Welcome to Employee Wage Computation Program"
 
 isPartTime=1;
 isFullTime=2;
+titalSalary=0;
 empRatePerHr=20;
-empCheck=$((RANDOM%2));
+numWorkingDays=20;
 
-case $empCheck in
-	1)
-	echo "Employee is present";
-	empHrs=8;
-	;;
+for (( day=1; day<=numWorkingDays; day++ ))
+do
+   empCheck=$((RANDOM%2));
 
-	2)
-	echo "Employee is working as part time";
-	empHrs=4;
-	;;
+	case $empCheck in
+		1)
+		echo "Employee is present";
+		empHrs=8;
+		;;
 
-	*)
-	empHrs=0
-	;;
+		2)
+		echo "Employee is working as part time";
+		empHrs=4;
+		;;
 
-esac
+		*)
+		empHrs=0
+		;;
 
-salary=$(($empHrs*$empRatePerHr));
+	esac
+
+	salary=$(($empHrs*$empRatePerHr));
+	totalSalary=$(($totalSalary+$salary));
+done
 
